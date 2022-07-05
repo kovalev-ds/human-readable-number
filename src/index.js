c84fec3f719e5ccc0cd31935f4aa07a8329d01d3
kovvdn <40830091+kovvdn@users.noreply.github.com>
2022-07-05 22:45:24 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function toReadable (number) {
-  
-}
+const units = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+];
+
+const dozens = [
+  "",
+  "",
+  "twenty",
+  "thirty",
+  "forty",
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety",
+];
+
+module.exports = function toReadable(number) {
+  if (number <= 19) return units[number];
+  if (number <= 99)
+    return number % 10 === 0
+      ? dozens[(number / 10) | 0]
+      : dozens[(number / 10) | 0] + " " + units[number % 10];
+  return number % 100 === 0
+    ? units[(number / 100) | 0] + " hundred"
+    : units[(number / 100) | 0] + " hundred " + toReadable(number % 100);
+};
